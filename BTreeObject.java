b0311e2fd6a228b6bda08376845ddcb73a740c27
AlexGuy94 <55674640+AlexGuy94@users.noreply.github.com>
2020-08-08 21:10:31 -0600

    Add files via upload

---
+++ BTreeObject.java
@@ -0,0 +1,47 @@
+/**
+ * CS321: Bioinformatics Group Project
+ * 
+ * BTreeObject class to store gene sequences
+ * 
+ * @author Alex Guy, Ryan Josephson, Andres Guzman
+ *
+ */
+ 
+ public class BTreeObject{
+ 
+ 
+ 
+	private int frequency;
+	private long key;
+	
+	public BTreeObject(long key){
+		
+		this.key = key;
+		frequency = 1;
+		
+	}
+	
+	
+	public getFrequency(){
+		
+		return frequency;
+	}
+	
+	public void setFrequency(int i){
+		
+		frequency = i;
+	}
+	
+	public void incrementFrequency(){
+		
+		frequency++;
+	}
+ 
+ 
+	public long getKey(){
+		
+		return key;
+	}
+ 
+ 
+ }
